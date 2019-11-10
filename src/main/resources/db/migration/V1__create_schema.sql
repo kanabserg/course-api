d3ac3bb8662946e188a79a700059f7f21b52f80c
kanabserg <jokbro163@gmail.com>
2019-11-10 22:07:00 +0400

    flyway and spring security

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -19,27 +19,3 @@ CREATE TABLE IF NOT EXISTS coursemanagement.course
   topic_id    BIGINT                        NOT NULL,
   CONSTRAINT course_topic_fk FOREIGN KEY (topic_id) REFERENCES coursemanagement.topic (id) ON DELETE CASCADE
 );
-
-DO
-$$
-  DECLARE
-    topic_id_spring BIGINT;
-    topic_id_jcore  BIGINT;
-    topic_id_js     BIGINT;
-  BEGIN
-    INSERT INTO coursemanagement.topic (name,description) VALUES ('Spring Framework', 'Spring Framework topic description')
-      RETURNING id INTO topic_id_spring;
-
-    INSERT INTO coursemanagement.topic (name,description) VALUES ('Core Java', 'Core Java topic description')
-      RETURNING id INTO topic_id_jcore;
-
-    INSERT INTO coursemanagement.topic (name, description)
-    VALUES ('JavaScript', 'JavaScript topic description')
-        RETURNING id INTO topic_id_js;
-
-    INSERT INTO coursemanagement.course (topic_id, name, description)
-    VALUES (topic_id_spring, 'Introduction', 'Spring Framework introduction'),
-           (topic_id_spring, 'Basics', 'Discussion about basics');
-
-  END
-$$;
